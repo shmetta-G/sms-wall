e9ba578bd8056bbcbd830838d3ea1e51a4f441f6
ohsab <ohsab@github.com>
2015-12-11 04:59:50 +0100

    bootstraped

--- berichten.php
+++ berichten.php
@@ -1,21 +1,19 @@
 <?php
-	$displayMessages = 10;
+	$displayMessages = 8;
 	$displayIndex=0;
 	$db = new SQLite3('db/db.sqlite');
 	$results = $db->query("SELECT * FROM (SELECT * FROM inbox ORDER BY UpdatedInDB DESC LIMIT 0,${displayMessages}) ORDER BY UpdatedInDB ASC");
 	while($row = $results->fetchArray()) {
-		if ($displayIndex < $displayMessages - $bigMessages) {
-			echo "<div class='message to'>";
 
-		}else{
-			echo "<div class='message big'>";
-		}
-		$splitted = explode(" ", $row['UpdatedInDB']);
-		echo "<span class=\"sms\">";
-		echo $row['TextDecoded'];
-		echo "</span>";
-		echo "<br />";
-		echo "<span class=\"timestamp\">" . $splitted[1] . "</span> ";
+		echo "<div class='panel panel-default'>";
+			echo "<div class='panel-body'>";
+				$splitted = explode(" ", $row['UpdatedInDB']);
+				echo "<span class=\"sms\">";
+				echo $row['TextDecoded'];
+				echo "</span>";
+				echo "<br />";
+				echo "<span class=\"timestamp\">" . $splitted[1] . "</span> ";
+			echo "</div>";
 		echo "</div>";
 		$displayIndex++;
 	}
