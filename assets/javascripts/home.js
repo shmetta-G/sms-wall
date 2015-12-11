d5a6db79f897cfea342ac0e3cd04ab3042a64627
ohsab <ohsab@github.com>
2015-12-11 05:21:37 +0100

    sotp

--- assets/javascripts/home.js
+++ assets/javascripts/home.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var refreshId = setInterval(function() {
         $("#msg-wrapper").load('berichten.php', function() {
-            $(".message").last()[0].scrollIntoView();
+            //$(".message").last()[0].scrollIntoView();
         });
     }, 1000);
     $.ajaxSetup({ cache: false });
