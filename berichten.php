5bfff77a0ac4fd4afb992ccba60c68713901ea49
ohsab <ohsab@github.com>
2015-12-14 05:55:35 +0100

    why

--- berichten.php
+++ berichten.php
@@ -1,5 +1,5 @@
 <?php
-	$displayMessages = 10;
+	$displayMessages = 20;
 	$displayIndex=0;
 	$db = new SQLite3('db/db.sqlite');
 	$results = $db->query("SELECT * FROM (SELECT * FROM inbox ORDER BY UpdatedInDB DESC LIMIT 0,${displayMessages}) ORDER BY UpdatedInDB ASC");
@@ -8,14 +8,16 @@
 		echo "<div class='panel panel-default'>";
 			echo "<div class='panel-body'>";
 				echo "<div class=\"thumbnail sms col-md-1\">";
-					echo "<img src=\"assets/images/msg.png\" alt=\"sms\">";
+					echo "<img src=\"http://babeholder.pixoil.com/img/70/70\">";
 				echo "</div>";
 				$splitted = explode(" ", $row['UpdatedInDB']);
-				echo "<span class=\"sms\">";
-				echo $row['TextDecoded'];
-				echo "</span>";
-				echo "<br />";
-				echo "<span class=\"timestamp\">" . $splitted[1] . "</span> ";
+				echo "<div class=\"col-md-10\">";
+					echo "<span class=\"sms\">";
+					echo $row['TextDecoded'];
+					echo "</span>";
+					echo "<br />";
+					echo "<span class=\"timestamp\">" . $splitted[1] . "</span> ";
+				echo "</div>";
 			echo "</div>";
 		echo "</div>";
 		$displayIndex++;
