5d8ba30d818932d5e3e3b345b8c28c2efcfca9b7
Feliciaan De Palmenaer <feliciaan@de-palmenaer.eu>
2015-12-15 19:16:45 +0100

    YOLO

--- sms-manueel/server.py
+++ sms-manueel/server.py
@@ -1,9 +1,27 @@
+from __future__ import unicode_literals
+
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 app = Flask(__name__)
 
-@app.route("/")
+import sys
+sys.setdefaultencoding('utf-8')
+
+Base = automap_base()
+engine = create_engine('sqlite:///../db/db.sqlite')
+# Reflect the tables
+Base.prepare(engine, reflect=True)
+
+Inbox = Base.classes.inbox
+
+session = Session(engine)
+
+@app.route('/')
 def root():
-    return "Hello World"
+    l = len(session.query(Inbox).all())
+    return "Hello World" + str(l)
 
 if __name__ == "__main__":
     app.run(debug=True)
